1922c94c8be2beb0e180e8ece81ef42c20a0cabb
David 'Digit' Turner <digit+github@google.com>
2024-09-23 20:12:26 +0200

    Make output_test.py runnable under Windows.
    Convert all tests to run under Windows properly, except
    the ones that depend on the detection of smart terminals,
    are there is no equivalent to the 'script' Unix command
    to do that properly for now.

--- misc/output_test.py
+++ misc/output_test.py
@@ -12,13 +12,19 @@ import sys
 import tempfile
 import unittest
 from textwrap import dedent
-from typing import Dict
+import typing as T
+
+_IS_WINDOWS = platform.system() == "Windows"
 
 default_env = dict(os.environ)
 default_env.pop("NINJA_STATUS", None)
 default_env.pop("CLICOLOR_FORCE", None)
 default_env["TERM"] = ""
 NINJA_PATH = os.path.abspath("./ninja")
+if _IS_WINDOWS:
+    NINJA_PATH += ".exe"
+
+assert os.path.exists(NINJA_PATH), f"Cannot find Ninja: {NINJA_PATH}"
 
 
 class BuildDir:
@@ -36,11 +42,56 @@ class BuildDir:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.d.cleanup()
 
+    def run_cmd(
+        self,
+        cmd_args: T.Sequence[str] = [],
+        env: T.Dict[str, str] = default_env,
+        cwd: T.Union[None, str] = "",
+    ) -> str:
+        """Run Ninja command and retrieve its output.
+
+        This runs the command directly, without trying to run it in
+        a pseudo smart terminal (pty on Posix, console on Windows).
+
+        Args:
+            cmd_args: A list of command-line arguments to pass to Ninja.
+            env: An optional alternative environment to run the command in.
+            cwd: An optional current directory to run the command in.
+                By default, it is run in the build directory's path.
+                Use "" (an empty string) to run in the current directory instead.
+        Returns:
+            the combined stdout and stderr as a string, with \r removed when
+            running on Windows.
+        """
+        ninja_cmd = [NINJA_PATH] + cmd_args
+        if cwd == "":
+            cwd = self.d.name
+        ret = subprocess.run(
+            ninja_cmd,
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            cwd=cwd,
+            env=env,
+        )
+        if ret.returncode != 0:
+            if ret.stdout is not None:
+                sys.stdout.write(ret.stdout)
+            if ret.stderr is not None:
+                sys.stdout.write(ret.stderr)
+            ret.check_returncode()
+        final_output = ""
+        for line in ret.stdout.splitlines(True):
+            if len(line) > 0 and line[-1] == "\r":
+                continue
+            final_output += line.replace("\r", "")
+        return final_output
+
     def run(
         self,
         flags: str = "",
         pipe: bool = False,
-        env: Dict[str, str] = default_env,
+        env: T.Dict[str, str] = default_env,
     ) -> str:
         ninja_cmd = "{} {}".format(NINJA_PATH, flags)
         try:
@@ -71,35 +122,77 @@ class BuildDir:
         return final_output
 
 
-def run(
+def run_plan(
+    build_ninja: str,
+    args: T.Sequence[str] = [],
+    env: T.Dict[str, str] = default_env,
+) -> str:
+    """Convenience function to run Ninja with a given build plan.
+
+    Everything is written to and runs in a temporary directory.
+
+    Args:
+        build_ninja: Content of the build.ninja file.
+        args: List of Ninja command arguments.
+        env: Optional environment dictionary to run the command in.
+
+    Returns:
+        the combined stdout + stderr for the command as a string,
+        with \r removed when running on Windows.
+    """
+    with BuildDir(build_ninja) as b:
+        return b.run_cmd(args, env)
+
+
+def run_with_color(
     build_ninja: str,
     flags: str = "",
-    pipe: bool = False,
-    env: Dict[str, str] = default_env,
+    env: T.Dict[str, str] = default_env,
 ) -> str:
+    """Same as run_plan(), but uses a pseudo smart terminal to run the command.
+
+    Useful to test smart terminal detection and status outputs.
+    Currently only works on Posix.
+
+    Args:
+        build_ninja: Content of the build.ninja file.
+        flags: Extra Ninja command-line arguments as a single string.
+        env: Optional environment to run the command in.
+
+    Returns:
+        Combined stdout + stderr output as a single string.
+    """
     with BuildDir(build_ninja) as b:
-        return b.run(flags, pipe, env)
+        return b.run(flags, False, env)
 
 
-@unittest.skipIf(
-    platform.system() == "Windows", "These test methods do not work on Windows"
-)
 class Output(unittest.TestCase):
-    BUILD_SIMPLE_ECHO = "\n".join(
-        (
-            "rule echo",
-            '  command = printf "do thing"',
-            "  description = echo $out",
-            "",
-            "build a: echo",
-            "",
-        )
+    ECHO_COMMAND = "echo" if _IS_WINDOWS else "printf"
+    BUILD_SIMPLE_ECHO = """\
+rule echo
+  command = {print} "do thing"
+  description = echo $out
+
+build a:echo
+""".format(
+        print=ECHO_COMMAND
     )
 
     def test_issue_1418(self) -> None:
-        self.assertEqual(
-            run(
-                """rule echo
+        if _IS_WINDOWS:
+            build_plan = r"""rule echo
+  command = cmd /c "sleep $delay && echo $out"
+  description = echo $out
+
+build a: echo
+  delay = 3
+build b: echo
+  delay = 2
+build c: echo
+  delay = 1
+"""
+        else:
+            build_plan = r"""rule echo
   command = sleep $delay && echo $out
   description = echo $out
 
@@ -109,18 +202,19 @@ build b: echo
   delay = 2
 build c: echo
   delay = 1
-""",
-                "-j3",
-            ),
-            """[1/3] echo c\x1b[K
+"""
+        expected_output = """[1/3] echo c
 c
-[2/3] echo b\x1b[K
+[2/3] echo b
 b
-[3/3] echo a\x1b[K
+[3/3] echo a
 a
-""",
-        )
+"""
+        self.assertEqual(run_plan(build_plan, ["-j3"]), expected_output)
 
+    @unittest.skipIf(
+        platform.system() == "Windows", "These test methods do not work on Windows"
+    )
     def test_issue_1214(self) -> None:
         print_red = """rule echo
   command = printf '\x1b[31mred\x1b[0m'
@@ -130,26 +224,26 @@ build a: echo
 """
         # Only strip color when ninja's output is piped.
         self.assertEqual(
-            run(print_red),
+            run_with_color(print_red),
             """[1/1] echo a\x1b[K
 \x1b[31mred\x1b[0m
 """,
         )
         self.assertEqual(
-            run(print_red, pipe=True),
+            run_plan(print_red),
             """[1/1] echo a
 red
 """,
         )
         # Even in verbose mode, colors should still only be stripped when piped.
         self.assertEqual(
-            run(print_red, flags="-v"),
+            run_with_color(print_red, flags="-v"),
             """[1/1] printf '\x1b[31mred\x1b[0m'
 \x1b[31mred\x1b[0m
 """,
         )
         self.assertEqual(
-            run(print_red, flags="-v", pipe=True),
+            run_plan(print_red, ["-v"]),
             """[1/1] printf '\x1b[31mred\x1b[0m'
 red
 """,
@@ -159,32 +253,39 @@ red
         env = default_env.copy()
         env["CLICOLOR_FORCE"] = "1"
         self.assertEqual(
-            run(print_red, pipe=True, env=env),
+            run_plan(print_red, [], env=env),
             """[1/1] echo a
 \x1b[31mred\x1b[0m
 """,
         )
 
     def test_issue_1966(self) -> None:
-        self.assertEqual(
-            run(
-                """rule cat
-  command = cat $rspfile $rspfile > $out
+        if _IS_WINDOWS:
+            # The copy command prints the path of each input file on new lines
+            # so redirect to NUL to get rid of this.
+            command = 'cmd /c "copy $rspfile + $rspfile $out > NUL"'
+            command_expanded = 'cmd /c "copy cat.rsp + cat.rsp a > NUL"'
+        else:
+            command = "cat $rspfile $rspfile > $out"
+            command_expanded = "cat cat.rsp cat.rsp > a"
+
+        build_plan = f"""rule cat
+  command = {command}
   rspfile = cat.rsp
   rspfile_content = a b c
 
 build a: cat
-""",
-                "-j3",
-            ),
-            """[1/1] cat cat.rsp cat.rsp > a\x1b[K
-""",
-        )
+"""
+        expected = f"[1/1] {command_expanded}\n"
+
+        with BuildDir(build_plan) as b:
+            self.assertEqual(b.run_cmd(["-j3"]), expected)
 
     def test_pr_1685(self) -> None:
         # Running those tools without .ninja_deps and .ninja_log shouldn't fail.
-        self.assertEqual(run("", flags="-t recompact"), "")
-        self.assertEqual(run("", flags="-t restat"), "")
+        with BuildDir("") as b:
+            self.assertEqual(b.run_cmd(["-t", "recompact"]), "")
+            self.assertEqual(b.run_cmd(["-t", "restat"]), "")
 
     def test_issue_2048(self) -> None:
         with tempfile.TemporaryDirectory() as d:
@@ -222,7 +323,7 @@ build a: cat
         )
         with b:
             self.assertEqual(
-                b.run("", pipe=True),
+                b.run_cmd([]),
                 dedent(
                     """\
                 [1/1] touch somewhere/out && echo "somewhere/out: extra" > somewhere_else/out.d
@@ -235,24 +336,34 @@ build a: cat
             )
 
     def test_status(self) -> None:
-        self.assertEqual(run(""), "ninja: no work to do.\n")
-        self.assertEqual(run("", pipe=True), "ninja: no work to do.\n")
-        self.assertEqual(run("", flags="--quiet"), "")
+        with BuildDir("") as b:
+            self.assertEqual(b.run_cmd(), "ninja: no work to do.\n")
+            self.assertEqual(b.run_cmd(["--quiet"]), "")
 
     def test_ninja_status_default(self) -> None:
         "Do we show the default status by default?"
+        with BuildDir(Output.BUILD_SIMPLE_ECHO) as b:
+            self.assertEqual(b.run_cmd([]), "[1/1] echo a\ndo thing\n")
+
+    @unittest.skipIf(
+        platform.system() == "Windows", "These test methods do not work on Windows"
+    )
+    def test_ninja_status_default_with_color(self) -> None:
+        "Do we show the default status by default?"
         self.assertEqual(
-            run(Output.BUILD_SIMPLE_ECHO), "[1/1] echo a\x1b[K\ndo thing\n"
+            run_with_color(Output.BUILD_SIMPLE_ECHO), "[1/1] echo a\x1b[K\ndo thing\n"
         )
 
     def test_ninja_status_quiet(self) -> None:
         "Do we suppress the status information when --quiet is specified?"
-        output = run(Output.BUILD_SIMPLE_ECHO, flags="--quiet")
-        self.assertEqual(output, "do thing\n")
+        with BuildDir(Output.BUILD_SIMPLE_ECHO) as b:
+            output = b.run_cmd(["--quiet"])
+            self.assertEqual(output, "do thing\n")
 
     def test_entering_directory_on_stdout(self) -> None:
-        output = run(Output.BUILD_SIMPLE_ECHO, flags="-C$PWD", pipe=True)
-        self.assertEqual(output.splitlines()[0][:25], "ninja: Entering directory")
+        with BuildDir(Output.BUILD_SIMPLE_ECHO) as b:
+            output = b.run_cmd(["-C", b.d.name], cwd=None)
+            self.assertEqual(output.splitlines()[0][:25], "ninja: Entering directory")
 
     def test_tool_inputs(self) -> None:
         plan = """
@@ -262,27 +373,28 @@ build out1 : cat in1
 build out2 : cat in2 out1
 build out3 : cat out2 out1 | implicit || order_only
 """
-        self.assertEqual(
-            run(plan, flags="-t inputs out3"),
-            """implicit
+        with BuildDir(plan) as b:
+            self.assertEqual(
+                b.run_cmd(["-t", "inputs", "out3"]),
+                """implicit
 in1
 in2
 order_only
 out1
 out2
 """,
-        )
+            )
 
-        self.assertEqual(
-            run(plan, flags="-t inputs --dependency-order out3"),
-            """in2
+            self.assertEqual(
+                b.run_cmd(["-t", "inputs", "--dependency-order", "out3"]),
+                """in2
 in1
 out1
 out2
 implicit
 order_only
 """,
-        )
+            )
 
         # Verify that results are shell-escaped by default, unless --no-shell-escape
         # is used. Also verify that phony outputs are never part of the results.
@@ -297,54 +409,69 @@ build out$ 3 : phony out$ 2
 build all: phony out$ 3
 """
 
-        # Quoting changes the order of results when sorting alphabetically.
-        self.assertEqual(
-            run(plan, flags="-t inputs all"),
-            f"""{quote}out 2{quote}
+        with BuildDir(plan) as b:
+            # Quoting changes the order of results when sorting alphabetically.
+            self.assertEqual(
+                b.run_cmd(["-t", "inputs", "all"]),
+                f"""{quote}out 2{quote}
 in1
 out1
 """,
-        )
+            )
 
-        self.assertEqual(
-            run(plan, flags="-t inputs --no-shell-escape all"),
-            """in1
+            self.assertEqual(
+                b.run_cmd(["-t", "inputs", "--no-shell-escape", "all"]),
+                """in1
 out 2
 out1
 """,
-        )
+            )
 
-        # But not when doing dependency order.
-        self.assertEqual(
-            run(plan, flags="-t inputs --dependency-order all"),
-            f"""in1
+            # But not when doing dependency order.
+            self.assertEqual(
+                b.run_cmd(["-t", "inputs", "--dependency-order", "all"]),
+                f"""in1
 out1
 {quote}out 2{quote}
 """,
-        )
+            )
 
-        self.assertEqual(
-            run(plan, flags="-t inputs --dependency-order --no-shell-escape all"),
-            f"""in1
+            self.assertEqual(
+                b.run_cmd(
+                    ["-t", "inputs", "--dependency-order", "--no-shell-escape", "all"]
+                ),
+                f"""in1
 out1
 out 2
 """,
-        )
+            )
 
-        self.assertEqual(
-            run(
-                plan,
-                flags="-t inputs --dependency-order --no-shell-escape --print0 all",
-            ),
-            f"""in1\0out1\0out 2\0""",
-        )
+            self.assertEqual(
+                b.run_cmd(
+                    [
+                        "-t",
+                        "inputs",
+                        "--dependency-order",
+                        "--no-shell-escape",
+                        "--print0",
+                        "all",
+                    ]
+                ),
+                f"""in1\0out1\0out 2\0""",
+            )
 
     def test_explain_output(self):
-        b = BuildDir(
-            """\
+        if _IS_WINDOWS:
+            command_source = 'cmd /c "if not exist $out (echo x > $out)"'
+            command_expanded = 'cmd /c "if not exist input (echo x > input)"'
+        else:
+            command_source = "[ -e $out ] || touch $out"
+            command_expanded = "[ -e input ] || touch input"
+
+        build_plan = f"""\
             build .FORCE: phony
             rule create_if_non_exist
-              command = [ -e $out ] || touch $out
+              command = {command_source}
               restat = true
             rule write
               command = cp $in $out
@@ -353,15 +480,14 @@ out 2
             build output : write mid
             default output
             """
-        )
-        with b:
+        with BuildDir(build_plan) as b:
             # The explain output is shown just before the relevant build:
             self.assertEqual(
-                b.run("-v -d explain"),
+                b.run_cmd(["-v", "-d", "explain"]),
                 dedent(
-                    """\
+                    f"""\
                 ninja explain: .FORCE is dirty
-                [1/3] [ -e input ] || touch input
+                [1/3] {command_expanded}
                 ninja explain: input is dirty
                 [2/3] cp input mid
                 ninja explain: mid is dirty
@@ -372,11 +498,11 @@ out 2
             # Don't print "ninja explain: XXX is dirty" for inputs that are
             # pruned from the graph by an earlier restat.
             self.assertEqual(
-                b.run("-v -d explain"),
+                b.run_cmd(["-v", "-d", "explain"]),
                 dedent(
-                    """\
+                    f"""\
                 ninja explain: .FORCE is dirty
-                [1/3] [ -e input ] || touch input
+                [1/3] {command_expanded}
                 """
                 ),
             )
